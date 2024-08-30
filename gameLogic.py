010ed86a7e85555091227938b93343504af71e7c
omijbhabha <omkarsingh5128@gmail.com>
2024-08-30 08:12:51 +0530

    removed a integrated file

--- gameLogic.py
+++
@@ -1,9 +0,0 @@
-def updateGameDetectives(detectives):
-    for detective in detectives:
-        detective.setNewNode()
-
-def updateGameMrX(mr_x):
-    print("MR X'S TURN!!!")
-    if mr_x.double_move_tickets>0:
-        mr_x.askForDoubleMove()
-    mr_x.setNewNode()
